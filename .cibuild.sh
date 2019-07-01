03d7792082adee1709b3c6ee4b16b409911389e0
Matt Layher <mdlayher@gmail.com>
2019-07-01 10:46:15 -0400

    .cibuild.sh: expand from license check, check for gofmt
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- .cibuild.sh
+++ .cibuild.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Scan each non-vendored Go source file for license.
 EXIT=0
 GOFILES=$(find . -name "*.go" | grep -v "./vendor/")
 
 for FILE in $GOFILES; do
+	# Scan each non-vendored Go source file for license.
 	BLOCK=$(head -n 1 $FILE)
-
 	if [[ "$BLOCK" != "// Copyright 20"* ]]; then
 		echo "file missing license: $FILE"
+		EXIT=1
+	fi
+
+	# Ensure each file is gofmt'd.
+	DIFF=$(gofmt -d -s $FILE)
+	if [ ! -z "$DIFF" ]; then
+		echo "file is not gofmt'd: $FILE"
 		EXIT=1
 	fi
 done
