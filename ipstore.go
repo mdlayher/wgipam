cc31cbcb5dcbc938dcdcce33dd1a028b816b6b74
Matt Layher <mdlayher@gmail.com>
2019-07-02 17:51:35 -0400

    wgipam: add IPStore and ipStore types
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

---
+++ ipstore.go
@@ -0,0 +1,77 @@
+package wgipam
+
+import (
+	"errors"
+	"net"
+	"sync"
+
+	"github.com/mikioh/ipaddr"
+)
+
+// An IPStore can allocate IP addresses. IPStore implementations should be
+// configured to only return IPv4 or IPv6 addresses: never both from the same
+// instance.
+type IPStore interface {
+	// Allocate allocates the next available IP address. It returns false
+	// if no more IP addresses are available.
+	Allocate() (ip *net.IPNet, ok bool, err error)
+}
+
+var _ IPStore = &ipStore{}
+
+// An ipStore is an in-memory IPStore implementation.
+type ipStore struct {
+	mu  sync.Mutex
+	c   *ipaddr.Cursor
+	out bool
+}
+
+// NewIPStore returns an IPStore which allocates IP addresses from memory.
+func NewIPStore(subnets []*net.IPNet) (IPStore, error) {
+	if len(subnets) == 0 {
+		return nil, errors.New("wgipam: NewIPStore requires one or more subnets")
+	}
+
+	ps := make([]ipaddr.Prefix, 0, len(subnets))
+	var isIPv6 bool
+	for i, s := range subnets {
+		// Do not allow mixed address families.
+		if i == 0 {
+			isIPv6 = s.IP.To4() == nil
+		}
+
+		if (isIPv6 && s.IP.To4() != nil) || (!isIPv6 && s.IP.To4() == nil) {
+			return nil, errors.New("wgipam: all IPStore subnets must be the same address family")
+		}
+
+		ps = append(ps, *ipaddr.NewPrefix(s))
+	}
+
+	return &ipStore{
+		c: ipaddr.NewCursor(ps),
+	}, nil
+}
+
+// Allocate implements IPStore.
+func (s *ipStore) Allocate() (*net.IPNet, bool, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.out {
+		// No more addresses to provide.
+		return nil, false, nil
+	}
+
+	p := s.c.Pos()
+	if s.c.Next() == nil {
+		// We've reached the end of the cursor; no more IPs.
+		s.out = true
+	}
+
+	// TODO(mdlayher): track the allocation in a map.
+
+	return &net.IPNet{
+		IP:   p.IP,
+		Mask: p.Prefix.Mask,
+	}, true, nil
+}
