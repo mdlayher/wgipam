162b7b8ea84ddb55140f0a1ab6356a0cd481c506
Matt Layher <mdlayher@gmail.com>
2019-09-01 11:52:40 -0400

    internal/wgipamd: refactor package main into new Server type
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- cmd/wgipamd/main.go
+++ cmd/wgipamd/main.go
@@ -26,20 +26,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
-	"net/http"
-	"net/http/pprof"
 	"os"
 	"os/signal"
-	"strings"
-	"time"
+	"sync"
 
-	"github.com/mdlayher/wgdynamic-go"
-	"github.com/mdlayher/wgipam"
 	"github.com/mdlayher/wgipam/internal/config"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"golang.org/x/sync/errgroup"
+	"github.com/mdlayher/wgipam/internal/wgipamd"
 )
 
 // cfgFile is the name of the default configuration file.
@@ -53,9 +45,11 @@ func main() {
 	)
 	flag.Parse()
 
+	ll := log.New(os.Stderr, "", log.LstdFlags)
+
 	if *initFlag {
 		if err := config.WriteDefault(cfgFile); err != nil {
-			log.Fatalf("failed to write default configuration: %v", err)
+			ll.Fatalf("failed to write default configuration: %v", err)
 		}
 
 		return
@@ -63,189 +57,40 @@ func main() {
 
 	f, err := os.Open(*cfgFlag)
 	if err != nil {
-		log.Fatalf("failed to open configuration file: %v", err)
+		ll.Fatalf("failed to open configuration file: %v", err)
 	}
 
 	cfg, err := config.Parse(f)
 	if err != nil {
-		log.Fatalf("failed to parse %q: %v", f.Name(), err)
+		ll.Fatalf("failed to parse %q: %v", f.Name(), err)
 	}
 	_ = f.Close()
 
-	log.Printf("starting with configuration file %q", f.Name())
+	ll.Printf("starting with configuration file %q", f.Name())
 
 	// Use a context to handle cancelation on signal.
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	var eg errgroup.Group
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
 
-	eg.Go(func() error {
+	go func() {
+		defer wg.Done()
+
 		// Wait for signals (configurable per-platform) and then cancel the
 		// context to indicate that the process should shut down.
 		sigC := make(chan os.Signal, 1)
 		signal.Notify(sigC, signals()...)
 
 		s := <-sigC
-		log.Printf("received %s, shutting down", s)
-
+		ll.Printf("received %s, shutting down", s)
 		cancel()
-		return nil
-	})
+	}()
 
-	// Set up Prometheus instrumentation using the typical Go collectors.
-	reg := prometheus.NewPedanticRegistry()
-	reg.MustRegister(
-		prometheus.NewGoCollector(),
-		prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
-	)
-
-	// Configure the HTTP debug server, if applicable.
-	if cfg.Debug.Address != "" {
-		l, err := net.Listen("tcp", cfg.Debug.Address)
-		if err != nil {
-			log.Fatalf("failed to start debug listener: %v", err)
-		}
-
-		log.Printf("starting debug listener on %q: prometheus: %v, pprof: %v",
-			cfg.Debug.Address, cfg.Debug.Prometheus, cfg.Debug.PProf)
-
-		// Serve requests until the context is canceled.
-		eg.Go(func() error {
-			<-ctx.Done()
-			return l.Close()
-		})
-
-		eg.Go(func() error {
-			mux := http.NewServeMux()
-
-			if cfg.Debug.Prometheus {
-				mux.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
-			}
-
-			if cfg.Debug.PProf {
-				mux.HandleFunc("/debug/pprof/", pprof.Index)
-				mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-				mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-				mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-				mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-			}
-
-			return serve(http.Serve(l, mux))
-		})
+	// Start the server's goroutines and run until context cancelation.
+	if err := wgipamd.NewServer(*cfg, ll).Run(ctx); err != nil {
+		ll.Fatalf("failed to run: %v", err)
 	}
-
-	// Serve on each specified interface.
-	for _, ifi := range cfg.Interfaces {
-		ll := log.New(os.Stderr, ifi.Name+": ", log.LstdFlags)
-
-		// Configure lease storage based on the input configuration.
-		store, err := newStore(ifi.Name, cfg.Storage, ll)
-		if err != nil {
-			ll.Fatalf("failed to configure lease storage: %v", err)
-		}
-		defer store.Close()
-
-		l, err := wgdynamic.Listen(ifi.Name)
-		if err != nil {
-			ll.Fatalf("failed to listen on %q: %v", ifi.Name, err)
-		}
-
-		ll.Printf("listening on %q, serving: %s",
-			l.Addr(), subnetsString(ifi.Subnets))
-
-		ip4s, ip6s, err := wgipam.DualStackIPAllocator(store, ifi.Subnets)
-		if err != nil {
-			ll.Fatalf("failed to create IP allocator: %v", err)
-		}
-
-		h := &wgipam.Handler{
-			Log:     ll,
-			IPv4:    ip4s,
-			IPv6:    ip6s,
-			Leases:  store,
-			Metrics: wgipam.NewHandlerMetrics(reg, ifi.Name),
-		}
-
-		s := &wgdynamic.Server{
-			Log:       ll,
-			RequestIP: h.RequestIP,
-		}
-
-		// Serve requests until the context is canceled.
-		eg.Go(func() error {
-			<-ctx.Done()
-			return s.Close()
-		})
-
-		eg.Go(func() error {
-			return serve(s.Serve(l))
-		})
-
-		// Purge expired leases at regular intervals.
-		eg.Go(func() error {
-			tick := time.NewTicker(10 * time.Second)
-			for {
-				select {
-				case <-ctx.Done():
-					return nil
-				case t := <-tick.C:
-					if err := store.Purge(t); err != nil {
-						ll.Printf("failed to purge expired data: %v", err)
-					}
-				}
-			}
-		})
-	}
-
-	if err := eg.Wait(); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
-
-// newStore configures a Store for the specified interface from
-// storage configuration.
-func newStore(ifi string, s config.Storage, ll *log.Logger) (wgipam.Store, error) {
-	switch {
-	case s.Memory:
-		ll.Println("using ephemeral in-memory storage")
-		return wgipam.MemoryStore(), nil
-	case s.File != "":
-		file := fmt.Sprintf("%s-%s", ifi, s.File)
-		ll.Printf("using file %q storage", file)
-		return wgipam.FileStore(file)
-	default:
-		return nil, fmt.Errorf("invalid storage configuration: %#v", s)
-	}
-}
-
-// subnetsString turns subnets into a comma-separated string.
-func subnetsString(subnets []*net.IPNet) string {
-	var ss []string
-	for _, s := range subnets {
-		ss = append(ss, s.String())
-	}
-
-	return strings.Join(ss, ", ")
-}
-
-// serve unpacks and handles certain network listener errors as appropriate.
-func serve(err error) error {
-	if err == nil {
-		return nil
-	}
-
-	nerr, ok := err.(*net.OpError)
-	if !ok {
-		return err
-	}
-
-	// Unfortunately there isn't an easier way to check for this, but
-	// we want to ignore errors related to the connection closing, since
-	// s.Close is triggered on signal.
-	if nerr.Err.Error() != "use of closed network connection" {
-		return err
-	}
-
-	return nil
 }
---
+++ internal/wgipamd/server.go
@@ -0,0 +1,251 @@
+// Copyright 2019 Matt Layher
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wgipamd
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/http/pprof"
+	"strings"
+	"time"
+
+	"github.com/mdlayher/wgdynamic-go"
+	"github.com/mdlayher/wgipam"
+	"github.com/mdlayher/wgipam/internal/config"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/sync/errgroup"
+)
+
+// A Server coordinates the goroutines that handle various pieces of the
+// wgipamd server.
+type Server struct {
+	cfg config.Config
+
+	ll  *log.Logger
+	reg *prometheus.Registry
+
+	eg *errgroup.Group
+}
+
+// NewServer creates a Server with the input configuration and logger. If ll
+// is nil, logs are discarded.
+func NewServer(cfg config.Config, ll *log.Logger) *Server {
+	if ll == nil {
+		ll = log.New(ioutil.Discard, "", 0)
+	}
+
+	// Set up Prometheus instrumentation using the typical Go collectors.
+	reg := prometheus.NewPedanticRegistry()
+	reg.MustRegister(
+		prometheus.NewGoCollector(),
+		prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
+	)
+
+	return &Server{
+		cfg: cfg,
+
+		ll:  ll,
+		reg: reg,
+
+		eg: &errgroup.Group{},
+	}
+}
+
+// Run runs the wgipamd server until the context is canceled.
+func (s *Server) Run(ctx context.Context) error {
+	// Configure the HTTP debug server, if applicable.
+	if err := s.runDebug(ctx); err != nil {
+		return fmt.Errorf("failed to start debug HTTP server: %v", err)
+	}
+
+	// Serve on each specified interface.
+	for _, ifi := range s.cfg.Interfaces {
+		// Configure storage based on the input configuration and then start
+		// the wg-dynamic server for this interface.
+		store, err := newStore(ifi.Name, s.cfg.Storage, s.ll)
+		if err != nil {
+			return fmt.Errorf("failed to configure storage: %v", err)
+		}
+		defer store.Close()
+
+		if err := s.runServer(ctx, ifi, store); err != nil {
+			return fmt.Errorf("failed to run wg-dynamic server: %v", err)
+		}
+	}
+
+	// Wait for all goroutines to be canceled and stopped successfully.
+	if err := s.eg.Wait(); err != nil {
+		return fmt.Errorf("failed to serve: %v", err)
+	}
+
+	return nil
+}
+
+// runServer runs a wg-dynamic server for a single interface using goroutines,
+// until ctx is canceled.
+func (s *Server) runServer(ctx context.Context, ifi config.Interface, store wgipam.Store) error {
+	l, err := wgdynamic.Listen(ifi.Name)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %q: %v", ifi.Name, err)
+	}
+
+	// Prepend the interface name to all logs for this server.
+	logf := func(format string, v ...interface{}) {
+		s.ll.Println(ifi.Name + ": " + fmt.Sprintf(format, v...))
+	}
+
+	logf("listening on %q, serving: %s",
+		l.Addr(), subnetsString(ifi.Subnets))
+
+	ip4s, ip6s, err := wgipam.DualStackIPAllocator(store, ifi.Subnets)
+	if err != nil {
+		return fmt.Errorf("failed to create IP allocator: %v", err)
+	}
+
+	h := &wgipam.Handler{
+		Log:     s.ll,
+		IPv4:    ip4s,
+		IPv6:    ip6s,
+		Leases:  store,
+		Metrics: wgipam.NewHandlerMetrics(s.reg, ifi.Name),
+	}
+
+	srv := &wgdynamic.Server{
+		Log:       s.ll,
+		RequestIP: h.RequestIP,
+	}
+
+	// Serve requests until the context is canceled.
+	s.eg.Go(func() error {
+		<-ctx.Done()
+		return srv.Close()
+	})
+
+	s.eg.Go(func() error {
+		return serve(srv.Serve(l))
+	})
+
+	// Purge expired leases at regular intervals.
+	s.eg.Go(func() error {
+		tick := time.NewTicker(10 * time.Second)
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			case t := <-tick.C:
+				if err := store.Purge(t); err != nil {
+					logf("failed to purge expired data: %v", err)
+				}
+			}
+		}
+	})
+
+	return nil
+}
+
+// runDebug runs a debug HTTP server using goroutines, until ctx is canceled.
+func (s *Server) runDebug(ctx context.Context) error {
+	d := s.cfg.Debug
+	if d.Address == "" {
+		// Nothing to do, don't start the server.
+		return nil
+	}
+
+	// Configure the HTTP debug server.
+	l, err := net.Listen("tcp", d.Address)
+	if err != nil {
+		return fmt.Errorf("failed to start debug listener: %v", err)
+	}
+
+	s.ll.Printf("starting debug listener on %q: prometheus: %v, pprof: %v",
+		d.Address, d.Prometheus, d.PProf)
+
+	// Serve requests until the context is canceled.
+	s.eg.Go(func() error {
+		<-ctx.Done()
+		return l.Close()
+	})
+
+	s.eg.Go(func() error {
+		mux := http.NewServeMux()
+
+		if d.Prometheus {
+			mux.Handle("/metrics", promhttp.HandlerFor(s.reg, promhttp.HandlerOpts{}))
+		}
+
+		if d.PProf {
+			mux.HandleFunc("/debug/pprof/", pprof.Index)
+			mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+			mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+			mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+			mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+		}
+
+		return serve(http.Serve(l, mux))
+	})
+
+	return nil
+}
+
+// newStore configures a Store for the specified interface from
+// storage configuration.
+func newStore(ifi string, s config.Storage, ll *log.Logger) (wgipam.Store, error) {
+	switch {
+	case s.Memory:
+		ll.Println("using ephemeral in-memory storage")
+		return wgipam.MemoryStore(), nil
+	case s.File != "":
+		file := fmt.Sprintf("%s-%s", ifi, s.File)
+		ll.Printf("using file %q storage", file)
+		return wgipam.FileStore(file)
+	default:
+		return nil, fmt.Errorf("invalid storage configuration: %#v", s)
+	}
+}
+
+// subnetsString turns subnets into a comma-separated string.
+func subnetsString(subnets []*net.IPNet) string {
+	var ss []string
+	for _, s := range subnets {
+		ss = append(ss, s.String())
+	}
+
+	return strings.Join(ss, ", ")
+}
+
+// serve unpacks and handles certain network listener errors as appropriate.
+func serve(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	nerr, ok := err.(*net.OpError)
+	if !ok {
+		return err
+	}
+
+	// Unfortunately there isn't an easier way to check for this, but
+	// we want to ignore errors related to the connection closing, since
+	// s.Close is triggered on signal.
+	if nerr.Err.Error() != "use of closed network connection" {
+		return err
+	}
+
+	return nil
+}
