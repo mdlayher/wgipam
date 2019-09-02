2721421a0c6979a85a2668d993e1d9cea8ebbbc4
Matt Layher <mdlayher@gmail.com>
2019-09-02 13:43:17 -0400

    wgipam: refactor out HTTPHandler type for debug API
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

---
+++ http.go
@@ -0,0 +1,131 @@
+// Copyright 2019 Matt Layher
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wgipam
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/pprof"
+	"path"
+	"sort"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// A HTTPHandler provides the HTTP debug API handler for wgipamd.
+type HTTPHandler struct {
+	h      http.Handler
+	stores map[string]Store
+}
+
+// NewHTTPHandler creates a HTTPHandler with the specified configuration.
+func NewHTTPHandler(
+	usePrometheus, usePProf bool,
+	reg *prometheus.Registry,
+	stores map[string]Store,
+) *HTTPHandler {
+	mux := http.NewServeMux()
+
+	h := &HTTPHandler{
+		h:      mux,
+		stores: stores,
+	}
+
+	// TODO(mdlayher): chances are pretty good that this will evolve into
+	// something more than a "debug" API; e.g. adding the ability to immediately
+	// revoke a lease and etc. This is something worth considering when adding
+	// functionality to the API. At this time there are no stability guarantees.
+
+	// Create a route to dump leases for each interface.
+	for k := range h.stores {
+		mux.HandleFunc("/leases/"+k, h.leases)
+	}
+
+	// Optionally enable Prometheus and pprof support.
+	if usePrometheus {
+		mux.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	}
+
+	if usePProf {
+		mux.HandleFunc("/debug/pprof/", pprof.Index)
+		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	}
+
+	return h
+}
+
+func (h *HTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Matching on "/" would produce an overly broad rule, so check manually
+	// here and indicate that this is the wgipamd service.
+	if r.URL.Path == "/" {
+		_, _ = io.WriteString(w, "wgipamd\n")
+		return
+	}
+
+	h.h.ServeHTTP(w, r)
+}
+
+// leases returns a JSON listing of the leases for an interface.
+func (h *HTTPHandler) leases(w http.ResponseWriter, r *http.Request) {
+	// Get the interface name from the last element of the path.
+	s, ok := h.stores[path.Base(r.URL.Path)]
+	if !ok {
+		// TODO(mdlayher): is this impossible?
+		http.NotFound(w, r)
+		return
+	}
+
+	leases, err := s.Leases()
+	if err != nil {
+		http.Error(
+			w,
+			// This is a debug API so it's probably okay to show Go errors.
+			fmt.Sprintf("failed to list leases: %v", err),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	// Sort leases by when they started.
+	sort.Slice(leases, func(i, j int) bool {
+		return leases[i].Start.Before(leases[j].Start)
+	})
+
+	// Unpack leases into JSON-friendly format.
+	out := make([]jsonLease, 0, len(leases))
+	for _, l := range leases {
+		out = append(out, jsonLease{
+			IPv4:   l.IPv4.String(),
+			IPv6:   l.IPv6.String(),
+			Start:  int(l.Start.Unix()),
+			Length: int(l.Length.Seconds()),
+		})
+	}
+
+	_ = json.NewEncoder(w).Encode(out)
+}
+
+// A jsonLease is the JSON representation of a Lease.
+type jsonLease struct {
+	IPv4   string `json:"ipv4"`
+	IPv6   string `json:"ipv6"`
+	Start  int    `json:"start"`
+	Length int    `json:"length"`
+}
---
+++ http_test.go
@@ -0,0 +1,116 @@
+// Copyright 2019 Matt Layher
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wgipam
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func TestHTTPHandler(t *testing.T) {
+	l := &Lease{
+		IPv4:   MustCIDR("192.0.2.0/32"),
+		IPv6:   MustCIDR("2001:db8::/128"),
+		Start:  time.Unix(1, 0),
+		Length: 10 * time.Second,
+	}
+
+	store := MemoryStore()
+	if err := store.SaveLease(0, l); err != nil {
+		t.Fatalf("failed to store lease: %v", err)
+	}
+
+	tests := []struct {
+		name, path string
+		ok         bool
+		leases     []jsonLease
+	}{
+		// We have some tests elswhere for more intricate tests such as
+		// Prometheus metrics usage, to these tests will mostly focus on
+		// the JSON API aspects of the HTTP debug handler.
+		{
+			name: "index",
+			path: "/",
+			ok:   true,
+		},
+		{
+			name: "unknown interface",
+			path: "/leases/wgnotexist0",
+		},
+		{
+			name: "leases OK",
+			path: "/leases/wg0",
+			ok:   true,
+			leases: []jsonLease{{
+				IPv4:   l.IPv4.String(),
+				IPv6:   l.IPv6.String(),
+				Start:  int(l.Start.Unix()),
+				Length: int(l.Length.Seconds()),
+			}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			srv := httptest.NewServer(NewHTTPHandler(
+				true, true, prometheus.NewRegistry(), map[string]Store{
+					"wg0": store,
+				},
+			))
+			defer srv.Close()
+
+			u, err := url.Parse(srv.URL)
+			if err != nil {
+				t.Fatalf("failed to parse URL: %v", err)
+			}
+			u.Path = tt.path
+
+			c := &http.Client{Timeout: 1 * time.Second}
+			res, err := c.Get(u.String())
+			if err != nil {
+				t.Fatalf("failed to HTTP GET: %v", err)
+			}
+			defer res.Body.Close()
+
+			wantCode := http.StatusNotFound
+			if tt.ok {
+				wantCode = http.StatusOK
+			}
+
+			if diff := cmp.Diff(wantCode, res.StatusCode); diff != "" {
+				t.Fatalf("unexpected HTTP status (-want +got):\n%s", diff)
+			}
+
+			if !tt.ok {
+				return
+			}
+
+			var leases []jsonLease
+			if err := json.NewDecoder(res.Body); err != nil {
+				t.Fatalf("failed to decode leases: %v", err)
+			}
+
+			if diff := cmp.Diff(tt.leases, leases); diff != "" {
+				t.Fatalf("unexpected leases(-want +got):\n%s", diff)
+			}
+		})
+	}
+}
